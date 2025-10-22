5acf9816161205eb563322a54d39e6e2f8532e5b
CAFÉ <pciskolargx@gmail.com>
2025-10-22 17:27:46 -0300

    aula 1 finalizada

--- ola.php
+++ ola.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primeiro Exemplo de PHP</title>
+    <style>
+        p{
+            color: lightblue;
+            box-shadow: 1px 1px 1px black;
+        }
+    </style>
 </head>
 <body>
     <h1>Olá, mundo!</h1>
