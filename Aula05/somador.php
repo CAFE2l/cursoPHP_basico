2683de22bf5c6f4b0f92021e8ccb3d9edd52cec3
CAFÉ <pciskolargx@gmail.com>
2025-10-22 18:09:13 -0300

    somador finalizado

---
+++ Aula05/somador.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num1 = 3;
+        $num2 = 2;
+        $soma = $num1 + $num2;
+        echo "A soma entre $num1 e $num2 é igual a $soma";
+    ?>
+</body>
+</html>
