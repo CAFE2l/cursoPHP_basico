c3b1c9b93bce432d8fdc04de5d31b7b64aefe434
CAFÉ <pciskolargx@gmail.com>
2025-10-22 17:14:34 -0300

    hello world

---
+++ ola.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Exemplo de PHP</title>
+</head>
+<body>
+    <h1>Olá, mundo!</h1>
+    <?php
+        echo "<p>Este é o meu primeiro script PHP!</p>";
+    ?>
+</body>
+</html>
