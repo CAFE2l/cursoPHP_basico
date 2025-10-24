b3addff59709fc7400d88f81e0cd0c64b31e5061
CAFÉ <pciskolargx@gmail.com>
2025-10-24 17:36:25 -0300

    Aula 07 finalizada

---
+++ Aula06/Ex004.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+    <?php
+       $x = "abc";
+       $$x = "def";
+       echo"O conteúdo da variável X é $x";
+       echo "A variável criada recebeu o valor $abc"; 
+    ?>
+</body>
+</html>
