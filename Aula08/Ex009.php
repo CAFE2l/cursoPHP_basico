9776275f21d972894b806fd2ba4371f782caf338
CAFÉ <pciskolargx@gmail.com>
2025-10-24 19:00:16 -0300

    parei no 9:57

---
+++ Aula08/Ex009.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+    <?php
+       $valor = $_GET["v"];
+       echo "<h2>O valor enviado foi $valor</h2>"
+    ?>
+    </div>
+</body>
+</html>
