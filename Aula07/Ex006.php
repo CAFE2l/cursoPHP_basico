4f3bc97536e6143c3c0e66cfa6dcf0d934f6b525
CAFÉ <pciskolargx@gmail.com>
2025-10-24 17:58:56 -0300

    ex006 finallizado

---
+++ Aula07/Ex006.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+    <?php
+    $a = 3;
+    $b = "3";
+    $r = ($a == $b) ? "SIM" : "NÃO";
+    echo "As variáveis A e B são idênticas? $r";
+    $r = ($a === $b) ? "SIM" : "NÃO";
+    echo "<br>As variáveis A e B são iguais e do mesmo tipo? $r";
+    ?>
+</body>
+</html>
