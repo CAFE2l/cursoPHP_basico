a3503d4c2a925d777bdb97ad869fd5f2cf6260a1
CAFÉ <pciskolargx@gmail.com>
2025-10-25 10:23:06 -0300

    aula10 criada

---
+++ Aula010/Ex014/Ex014.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $num = isset($_GET["num"]) ? $_GET["num"]:0;
+        $oper = isset($_GET["oper"]) ? $_GET["oper"]:"1";
+        switch ($oper){
+            case 1:
+                $resultado  = $num * 2;
+                break;
+            case 2: 
+                $resultado = $num ^ 3;
+                break;
+            case 3: 
+                $resultado = sqrt($num);
+                break;
+            default:
+                $resultado = 0;
+                break;
+        }
+
+
+        echo "O resultado a operação solicitada é $resultado";
+
+    ?>
+
+    <a href="index.html">Voltar</a>
+</body>
+</html>
