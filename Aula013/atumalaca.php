b8cf5c236abdbf0a94cb51149f9e9c8acd1f0987
CAFÉ <pciskolargx@gmail.com>
2025-10-26 09:00:10 -0300

    atumalaca

---
+++ Aula013/atumalaca.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $num = isset($_GET["num"]) ? $_GET["num"] : 1;
+        $resultado = 0;
+        for ($i = 1; $i <= 10; $i++){
+            $resultado = $num * $i;
+            echo "$num x $i = $resultado<br>";
+        }
+    ?>
+</body>
+</html>
--- Aula013/test.php
+++ Aula013/test.php
@@ -9,12 +9,22 @@
    <?php 
     for($i = 1; $i <= 10; $i++){
         echo "$i "; 
-    echo"<br>";
-
+    }
+     echo"<br>";
     for ($i = 10; $i > 1; $i--){
         echo "$i ";
-    }
+        
+    } 
+     echo"<br>";
+    for ($i = 0; $i <= 100; $i += 5){
+        echo "$i ";
     }   
+
+    echo "<br>";
+    
+    for($i = 20; $i >= 0; $i -=2){
+        echo "$i ";
+    }
    ?> 
 </body>
 </html>
